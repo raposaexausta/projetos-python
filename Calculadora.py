cc9859c2bb329510126cc6f729b6330f3c1e7d84
Cristine Soares (Tina) <cristinecsoares@gmail.com>
2025-05-29 21:51:19 -0300

    Primeira versão da calculadora

---
+++ Calculadora.py
@@ -0,0 +1,44 @@
+while True:
+    num1 = input ('Digite o primeiro número: ')
+    num2 = input('Digite o segundo número: ')
+    operador = input('Digite o operador (+-*/): ')
+
+
+    num1_float = 0
+    num2_float = 0
+
+    try:
+        num1_float = float(num1)
+        num2_float = float(num2)
+    except:
+        print('Digite um número válido ')
+        continue
+
+    operadores_validos = '+=*/'
+
+    if operador not in operadores_validos:
+        print('Operador invalido')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador')
+        continue
+
+    print('Realizando conta; Confira o resultado: ')
+    if operador == '+':
+        print(f'{num1_float} + {num2_float} = ', num1_float + num2_float)
+    elif operador == '-':
+        print(f'{num1_float} - {num2_float} = ', num1_float - num2_float)
+    elif operador == '*':
+        print(f'{num1_float} * {num2_float} = ', num1_float * num2_float)
+    elif operador == '/':
+        print(f'{num1_float} / {num2_float} = ', num1_float / num2_float)
+
+
+    sair = input("Deseja sair? ").lower().startswith('s')
+
+    if sair is True:
+        break
+
+
+
